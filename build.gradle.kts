eb7616ea57efd174244de307925848bbaaa21827
Eunseok <egg6909@gmail.com>
2022-08-29 00:13:00 +0900

    library version updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,8 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven(url = "https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven(url = "https://androidx.dev/storage/compose-compiler/repository/")
     }
 
     tasks.withType<Test> {
